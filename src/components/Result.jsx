332a55a7157659d8e6fffb11782df0413fe99e82
NelsonElvis <nelsonelvisalarconcahuana@gmail.com>
2021-02-05 18:34:26 -0500

    modificaciones

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,11 @@
 const Resultado = ({total, plazo, cantidad}) =>( 
 <div className="u-full-width resultado">
     <h2>Resumen</h2>
-    <p>La cantidad solicitada es $ {cantidad}</p>
+    <p>La cantidad solicitada es S/ {cantidad}</p>
     <p>A pagar en: {plazo} meses</p>
-    <p>Su pago mensual es de: $ {(total/plazo).toFixed(2)}</p>
-    <p>Total a pagar: $ {(total).toFixed(2)}</p>
+    <p>Su pago mensual es de: S/ {(total/plazo).toFixed(2)}</p>
+    <p>Total a pagar: S/ {(total).toFixed(2)}</p>
+    <h6>Alex kbro</h6>
 </div>
  );
 
